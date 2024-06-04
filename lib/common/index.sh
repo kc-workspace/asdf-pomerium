230c67967686a82f159881e37f1a18443ea5fa3a
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2024-06-04 13:49:54 +0700

    perf: update plugin from template [autocommit]

---
+++ lib/common/index.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+## Environment variables
+## https://asdf-vm.com/plugins/create.html#environment-variables-overview
+
+## General information
+KC_ASDF_RES_PATH="${KC_ASDF_PLUGIN_PATH:?}/res"
+KC_ASDF_ADDON_LIST=""
+export KC_ASDF_RES_PATH KC_ASDF_ADDON_LIST
+
+## Plugin information
+KC_ASDF_ORG="kc-workspace"
+KC_ASDF_NAME="asdf-pomerium"
+KC_ASDF_REPO="https://github.com/kc-workspace/asdf-pomerium"
+export KC_ASDF_ORG KC_ASDF_NAME KC_ASDF_REPO
+
+## Application information
+KC_ASDF_APP_TYPE="standalone"
+KC_ASDF_APP_NAME="pomerium-cli"
+KC_ASDF_APP_WEBS="https://www.pomerium.com"
+KC_ASDF_APP_DESC="it's used to generate encrypted and authenticated connections for services that are not accessed through a web browser"
+KC_ASDF_APP_CMDS="
+- test    - test if cli exist and executable
+- version - print *plugin* version
+"
+KC_ASDF_APP_REPO="https://github.com/pomerium/cli"
+export KC_ASDF_APP_TYPE KC_ASDF_APP_NAME
+export KC_ASDF_APP_WEBS KC_ASDF_APP_DESC
+export KC_ASDF_APP_CMDS KC_ASDF_APP_REPO
+
+# shellcheck source-path=SCRIPTDIR/internal.sh
+source "${KC_ASDF_PLUGIN_PATH:?}/lib/common/internal.sh" || exit 1
+# shellcheck source-path=SCRIPTDIR/defaults.sh
+source "${KC_ASDF_PLUGIN_PATH:?}/lib/common/defaults.sh" || exit 1
+
+kc_asdf_commands go
